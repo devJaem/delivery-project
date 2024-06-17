64f0ddcb2925cf164b40b5412d890ebb99c6b9cd
jaemin <wjdwoals9292@gmail.com>
2024-06-17 23:07:52 +0900

    update: auth-logout-bugfix

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -94,9 +94,8 @@ class AuthService {
     if (!result) {
       throw new BadRequestError(MESSAGES.AUTH.COMMON.JWT.NO_USER);
     }
-    return result;
+    return { userId };
   };
-  
 }
 
 export default AuthService;
