3e08cf9bd92244087b85c9c7cc5005d403df74b0
jaemin <wjdwoals9292@gmail.com>
2024-06-20 21:37:23 +0900

    hotfix: revenue 수정

--- src/repositories/restaurant.repository.js
+++ src/repositories/restaurant.repository.js
@@ -31,7 +31,6 @@ class RestaurantRepository {
         category,
         description,
         restaurantPicture,
-        revenue: 0,
         owner: {
           connect: {
             userId: user.userId,
